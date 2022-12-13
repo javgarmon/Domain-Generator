93ff0a23562c2ec9377296627c6fa9f05630d55a
javgarmon <javier.garces.m@gmail.com>
2022-12-13 19:27:03 +0000

    first push

--- src/app.js
+++ src/app.js
@@ -7,5 +7,20 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  var pronoun = ["the", "our"];
+  var adj = ["great", "big"];
+  var noun = ["jogger", "racoon"];
+  var extension = [".net", ".com", ".es", ".org"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < extension.length; l++) {
+          console.log(pronoun[i] + adj[j] + noun[k] + extension[l]);
+        }
+      }
+    }
+  }
+
   console.log("Hello Rigo from the console!");
 };
